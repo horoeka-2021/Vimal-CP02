399723d1709f0bbb5de238169ba1eef0f6894544
Vimal <46978245+vimal-raj-nagarajan@users.noreply.github.com>
2021-10-18 19:05:08 +1300

    Initial commit. CP02

---
+++ index.js
@@ -0,0 +1,15 @@
+const superheroes = require('superheroes')
+
+const sh1 = superheroes.random();
+const sh2 = superheroes.random();
+const sh3 = superheroes.random();
+const sh4 = superheroes.random();
+const sh5 = superheroes.random();
+
+
+//Five random superhero names
+console.log(sh1);
+console.log(sh2);
+console.log(sh3);
+console.log(sh4);
+console.log(sh5);
